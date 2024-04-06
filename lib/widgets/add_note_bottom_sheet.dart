c9a479a5cadc8aaccc603ad53097521d81e4a553
Your Name <you@example.com>
2024-04-06 15:21:41 +0200

    take input and validate data

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -11,30 +11,68 @@ class AddNoteBottomSheet extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 32,
-            ),
-            CustomTextField(
-              hintText: "Title",
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            CustomTextField(
-              hintText: "Content",
-              maxLines: 5,
-            ),
-            SizedBox(
-              height: 35,
-            ),
-            CustomButton(),
-            SizedBox(
-              height: 32,
-            ),
-          ],
-        ),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, content;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            hintText: "Title",
+            onSaved: (value) {
+              title = value;
+            },
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            hintText: "Content",
+            onSaved: (value) {
+              content = value;
+            },
+            maxLines: 5,
+          ),
+          const SizedBox(
+            height: 35,
+          ),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always; 
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+        ],
       ),
     );
   }
